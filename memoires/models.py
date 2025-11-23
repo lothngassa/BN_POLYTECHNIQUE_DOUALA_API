761127cd7cd4174c9ccad1efa71c9ce33136ef14
Loth Ngassa <ngassayimgal@gmail.com>
2025-11-23 16:16:30 +0100

    Update models.py

--- memoires/models.py
+++ memoires/models.py
@@ -1,18 +1,14 @@
 from django.db import models
 
 class Memoire(models.Model):
-    """
-    Modèle pour enregistrer les mémoires avec stockage du fichier dans Supabase.
-    """
     titre = models.CharField(max_length=255)
     auteur = models.CharField(max_length=150)
     annee = models.IntegerField()
-
     filiere = models.CharField(max_length=50)
     axe_recherche = models.CharField(max_length=100, blank=True, null=True)
 
-    # On stocke maintenant seulement le lien Supabase
-    fichier_pdf_url = models.URLField(max_length=500, verbose_name="URL du PDF Supabase")
+    # Supabase URL ici
+    fichier_pdf = models.URLField(max_length=500)
 
     date_soumission = models.DateTimeField(auto_now_add=True)
 
@@ -21,4 +17,5 @@ class Memoire(models.Model):
 
     def __str__(self):
         return f"{self.titre} par {self.auteur} ({self.annee})"
+
 
