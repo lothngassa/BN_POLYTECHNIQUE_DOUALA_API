8ecefd6135d837b68a5cc27673d5fa4e8e7d8c47
Loth Ngassa <ngassayimgal@gmail.com>
2025-11-23 16:04:02 +0100

    Update serializers.py

--- memoires/serializers.py
+++ memoires/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 from .models import Memoire
 
-
 class MemoireSerializer(serializers.ModelSerializer):
     class Meta:
         model = Memoire
-        # Assurez-vous que tous les champs de votre modèle sont listés ici
-        fields = ['id', 'titre', 'auteur', 'annee', 'filiere', 'fichier_pdf']
+        fields = [
+            'id', 'titre', 'auteur', 'annee',
+            'filiere', 'axe_recherche',
+            'fichier_pdf', 'date_soumission'
+        ]
 
-        # --- LIGNE CLÉ AJOUTÉE POUR LE DÉPLOIEMENT/TEST ---
-        # Ceci permet d'omettre le fichier PDF lors de la création d'un test
         extra_kwargs = {
-            'fichier_pdf': {'required': False}
+            'fichier_pdf': {'required': True}   # il FAUT un fichier
         }
-        # ----------------------------------------------------
